053aa89369cb64c0a851594e379c5dafbb89d39e
Zach Bimson <zach.bimson@gmail.com>
2016-03-10 20:37:35 +0000

    Class updates
    Fleshed out getMarkup()
    added array merges to construct
    added default links
    various tidy ups

--- src/Plugin/Social/SocialLinks.php
+++ src/Plugin/Social/SocialLinks.php
@@ -7,7 +7,7 @@
 
 namespace Drupal\social_links\Plugin\Social;
 
-use \Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 
 /**
  * Plugin implementation of the 'social_links' field type.
@@ -17,13 +17,13 @@ use \Drupal\Core\Extension\ModuleHandlerInterface;
 class SocialLinks {
 
   private $social_links = [];
-  private $theme_function = 'social_links_theme';
+  private $theme_function = 'item_list';
 
-  public function __construct(&$links, $override = false) {
-    $links = [] + $links;
+  public function __construct(&$links = [], &$override = false) {
+    $links = array_merge([], $links);
 
     if (!$override && is_array($links)) {
-      $links = $this->getDefaults() + $links;
+      $links = array_merge($this->getDefaults(), $links);
     }
 
     $this->registerLinks($links);
@@ -31,10 +31,10 @@ class SocialLinks {
   }
 
   public function registerLinks($links) {
-    $module_handler = \Drupal::ModuleHandlerInterface();
-    $hook_links = $module_handler->invokeAll('hook_social_links_alter', $links);
+    $module_handler = \Drupal::moduleHandler();
+    $hook_links = $module_handler->invokeAll('social_links_alter', array($links));
 
-    if (!empty($hook_links) {
+    if (!empty($hook_links)) {
       $links = $hook_links;
     }
 
@@ -43,8 +43,12 @@ class SocialLinks {
   }
 
   public function getDefaults() {
-    // supply defaults
-    return [];
+    return [
+      'twitter' => ['callback' => 'social_links_provider_popup'],
+      'facebook' => ['callback' => 'social_links_provider_popup'],
+      'googleplus' => ['callback' => 'social_links_provider_popup'],
+      'email' => ['callback' => 'social_links_provider_email'],
+    ];
   }
 
   public function addLinks($links) {
@@ -55,22 +59,41 @@ class SocialLinks {
 
   public function renderLinks($entity, &$theme_override = '') {
     return array(
-      '#markup' => $this->getMarkup($entity),
+      '#items' => $this->getMarkup($entity),
       '#theme' => $this->getTheme($theme_override),
     );
   }
 
   public function getMarkup($entity) {
+    $markup_array = [];
     $links = $this->getLinks();
+    // Make external link
+    $url = $entity->toUrl('canonical')->toString();
 
     // look at nice way to attch js, maybe to theme array
     foreach ($links as $provider => $config) {
-      // get entity info
+
       // check in about svg support/ contrib module support
-      // build links array
-      // run call backs
+
+      // Investigate D8 link class or correct way of passing to link theme
+      $link = [
+        '#theme' => 'link',
+        '#path' => $url,
+        '#text' => $provider,
+        '#options' => [
+          'attributes' => ['class' => ['social-link', $provider]],
+          'html' => TRUE,
+        ],
+      ];
+
+      if (isset($config['callback'])) {
+        $config['callback']($link, $entity);
+      }
+
+      $markup_array[] = $link;
     }
-    // return links array
+
+    return $markup_array;
   }
 
   public function getLinks() {
@@ -78,7 +101,7 @@ class SocialLinks {
   }
 
   public function getTheme(&$override) {
-    if (!is_null($override)) {
+    if (!empty($override)) {
       $this->theme_function = $override;
       return $override;
     }
