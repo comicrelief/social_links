f00b82c7f5199946d623c157a89224b426462a5f
Zach Bimson <zach.bimson@gmail.com>
2016-03-10 10:48:03 +0000

    Updated SocialLinks class - added new methods
    Added:
     - registerLinks()
     - addLinks()
     - renderLinks()
     - getLinks()
    
    Updated:
     - __construct()
     - getTheme()

--- src/Plugin/Social/SocialLinks.php
+++ src/Plugin/Social/SocialLinks.php
@@ -7,6 +7,8 @@
 
 namespace Drupal\social_links\Plugin\Social;
 
+use \Drupal\Core\Extension\ModuleHandlerInterface;
+
 /**
  * Plugin implementation of the 'social_links' field type.
  *
@@ -15,17 +17,72 @@ namespace Drupal\social_links\Plugin\Social;
 class SocialLinks {
 
   private $social_links = [];
+  private $theme_function = 'example_social_theme_callback';
 
-  public function __construct() {
+  public function __construct(&$links, $override = false) {
+    $links = [] + $links;
+
+    if (!$override && is_array($links)) {
+      $links = $this->getDefaults() + $links;
+    }
+
+    $this->registerLinks($links);
 
   }
 
-  public function getMarkup() {
+  public function registerLinks($links) {
+    $module_handler = \Drupal::ModuleHandlerInterface();
+    $hook_links = $module_handler->invokeAll('hook_social_links_alter', $links);
+
+    if (!empty($hook_links) {
+      $links = $hook_links;
+    }
+
+    $this->addLinks($links);
 
   }
 
-  public function getTheme() {
-    
+  public function getDefaults() {
+    // supply defaults
+    return [];
+  }
+
+  public function addLinks($links) {
+    if (is_array($links)) {
+      $this->social_links = $this->social_links + $links;
+    }
+  }
+
+  public function renderLinks($entity, &$theme_override = '') {
+    return array(
+      '#markup' => $this->getMarkup($entity),
+      '#theme' => $this->getTheme($theme_override),
+    );
+  }
+
+  public function getMarkup($entity) {
+    $links = $this->getLinks();
+
+    // look at nice way to attch js, maybe to theme array
+    foreach ($links as $provider => $config) {
+      // get entity info
+      // check in about svg support/ contrib module support
+      // build links array
+      // run call backs
+    }
+    // return links array
+  }
+
+  public function getLinks() {
+    return $this->social_links;
+  }
+
+  public function getTheme(&$override) {
+    if (!is_null($override)) {
+      $this->theme_function = $override;
+      return $override;
+    }
+    return $this->theme_function;
   }
 
 }
