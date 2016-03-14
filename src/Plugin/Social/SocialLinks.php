e7f8f6dd8b44627b2311bf25f0abb8ee6ab6b433
Zach Bimson <zach.bimson@gmail.com>
2016-03-14 22:07:47 +0000

    Updated defaults to include path and bulked out getMarkup()

--- src/Plugin/Social/SocialLinks.php
+++ src/Plugin/Social/SocialLinks.php
@@ -8,6 +8,7 @@
 namespace Drupal\social_links\Plugin\Social;
 
 use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Url;
 
 /**
  * Plugin implementation of the 'social_links' field type.
@@ -44,10 +45,22 @@ class SocialLinks {
 
   public function getDefaults() {
     return [
-      'twitter' => ['callback' => 'social_links_provider_popup'],
-      'facebook' => ['callback' => 'social_links_provider_popup'],
-      'googleplus' => ['callback' => 'social_links_provider_popup'],
-      'email' => ['callback' => 'social_links_provider_email'],
+      'twitter' => [
+        'callback' => 'social_links_provider_popup',
+        'path' => 'http://twitter.com/home?status=',
+      ],
+      'facebook' => [
+        'callback' => 'social_links_provider_popup',
+        'path' => 'https://www.facebook.com/sharer/sharer.php?u=',
+      ],
+      'googleplus' => [
+        'callback' => 'social_links_provider_popup',
+        'path' => 'https://plus.google.com/share?url=',
+      ],
+      'email' => [
+        'callback' => 'social_links_provider_email',
+        'path' => '',
+      ],
     ];
   }
 
@@ -66,29 +79,40 @@ class SocialLinks {
 
   public function getMarkup($entity) {
     $markup_array = [];
+
     $links = $this->getLinks();
-    // Make external link
-    $url = $entity->toUrl('canonical')->toString();
+
+    $request = \Drupal::request();
+    $route_match = \Drupal::routeMatch();
+    $page_title = urlencode(\Drupal::service('title_resolver')->getTitle($request, $route_match->getRouteObject()));
+    $entity_url = urlencode($request->getUri());
 
     // look at nice way to attch js, maybe to theme array
     foreach ($links as $provider => $config) {
 
       // check in about svg support/ contrib module support
 
-      // Investigate D8 link class or correct way of passing to link theme
-      $link = [
-        '#theme' => 'link',
-        '#path' => $url,
-        '#text' => $provider,
-        '#options' => [
-          'attributes' => ['class' => ['social-link', $provider]],
-          'html' => TRUE,
+      $link_title = ucfirst($provider);
+      $link_class = $provider . '-social-link';
+      $link_options = [
+        'path' => $config['path'] . $entity_url . '&amp;title=' . $page_title,
+        'attributes' => [
+          'title' => $link_title,
+          'class' => [
+            'social-link',
+            $link_class,
+          ],
         ],
       ];
 
+
       if (isset($config['callback'])) {
-        $config['callback']($link, $entity);
+        $config['callback']($link_options, $entity);
       }
+
+      $url = Url::fromUri($link_options['path']);
+      $url->setOptions($link_options);
+      $link = \Drupal::l(t($link_title), $url);
 
       $markup_array[] = $link;
     }
